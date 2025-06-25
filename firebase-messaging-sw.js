5fff7325d4b31146f8b90162cf11b6844b418e6e
Aayush-afk1 <14ryayush9@gmail.com>
2025-06-25 13:46:40 +0545

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,11 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCg06rK2yDeHdoeg4D4NJb3TYjlwqQGKdw",
   authDomain: "aakriti-cbde0.firebaseapp.com",
   projectId: "aakriti-cbde0",
-  storageBucket: "aakriti-cbde0.appspot.com",
+  storageBucket: "aakriti-cbde0.firebasestorage.app",
   messagingSenderId: "543051670501",
   appId: "1:543051670501:web:6c8591fe0878622473c50c",
   measurementId: "G-SNT13V6DDR"
@@ -14,9 +14,13 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log("[firebase-messaging-sw.js] Received background message", payload);
-  self.registration.showNotification(payload.notification.title, {
+  console.log('[firebase-messaging-sw.js] Received background message', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: "/ayush-app/icons/icon-192.png"
-  });
+    icon: payload.notification.icon || "/icons/icon-192x192.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello from Aakriti 🎉</h1>
-    <p>This app supports push notifications and can be installed as a PWA.</p>
+    <App />
   </React.StrictMode>
 );
